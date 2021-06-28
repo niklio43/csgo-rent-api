48b5a51f3a2c65f1000c82458cac79320dcaa764
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-06-28 10:30:21 +0200

    fixing search method

--- API/Skins/searchSkin.php
+++ API/Skins/searchSkin.php
@@ -1,38 +1,40 @@
 <?php
-//Headers
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 include_once '../../Config/Database.php';
 include_once '../../Models/search.php';
 
-//Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
-//Instantiate user object
 $search = new Search($db);
 
-//Get name from URL
-$search->name = isset($_GET['name']) ? $_GET['name'] : die();
+$search->search = isset($_GET['search']) ? $_GET['search'] : die('Test2');
 
-//Get skin data
-$search->searchSkin();
+$result = $search->searchSkin();
 
-//Create array
-$search_arr = array(
-    'skinID' => $search->skinID,
-    'name' => $search->name,
-    'market_name' => $search->market_name,
-    'icon_url' => $search->icon_url,
-    'link' => $search->link,¨
-    'price' => $search->price,
-    'steamID' => $search->steamID,
-    'float_value' => $search->float_value,
-    'patternSeed' => $search->patternSeed
-);
+$rowCount = $result->rowCount();
 
-//Make JSON
-print_r(json_encode($search_arr));
+if($rowCount > 0){
+    $search_arr = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $search_item = array(
+            'name' => $name
+        );
+
+        array_push($search_arr, $search_item);
+    }
+
+    echo json_encode($search_arr);
+
+} else {
+    echo json_encode(
+        array('message' => 'No Skins Found')
+    );
+}
 
 ?>
--- Models/search.php
+++ Models/search.php
@@ -6,33 +6,21 @@ class Search {
         private $table = 'skin';
     
         public $name;
+        public $search;
 
         public function __construct($db) {
             $this->conn = $db;
         }
 
         public function searchSkin(){
-            $sql = "SELECT * FROM `skin` WHERE name LIKE '%AUG%'";
-        
-            $stmt = $this->conn->prepare($sql);
+            $sql = 'SELECT * FROM ' . $this->table . ' WHERE name LIKE "%'.$this->search.'%"';
 
             $stmt = $this->conn->prepare($sql);
-
-            $stmt->bindParam(1, $this->name);
 
             $stmt->execute();
 
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $this->skinID = $row['skinID'];
-            $this->name = $row['name'];
-            $this->market_name = $row['market_name'];
-            $this->icon_url = $row['icon_url'];
-            $this->link = $row['link'];
-            $this->price = $row['price'];
-            $this->steamID = $row['steamID'];
-            $this->float_value = $row['float_value'];
-            $this->patternSeed = $row['patternSeed'];
+            return $stmt;
+            
         }
     }
 
