8f6ce562c2032e335b561dd11ced1406746ba68e
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-07-09 01:04:36 +0200

    Update searchSkin.php

--- API/Skins/searchSkin.php
+++ API/Skins/searchSkin.php
@@ -15,9 +15,15 @@ $skin->name = isset($_GET['name']) ? $_GET['name'] : die();
 
 $skin->searchSkin();
 
-$skin_arr = array(
-    'name' => $skin->name,
-);
+$skin_arr = array();
+
+$skin_arr['data'] = array();
+
+    $skin_item = array(
+	'name' => $skin->name
+    );
+
+    array_push($skin_arr['data'], $skin_item);
 
 print_r(json_encode($skin_arr));
 
