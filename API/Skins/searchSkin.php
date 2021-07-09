bb478416fad60084388d617707de727cd4f822df
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-07-09 18:11:11 +0200

    Update searchSkin.php

--- API/Skins/searchSkin.php
+++ API/Skins/searchSkin.php
@@ -13,17 +13,11 @@ $skin = new Skin($db);
 
 $skin->name = isset($_GET['name']) ? $_GET['name'] : die();
 
-$result = $skin->searchSkin();
+$skin->searchSkin();
 
-$num = $result->rowCount();
+$skin_arr = array();
 
-if($num > 0){
-
-    $skin_arr = array();
-    $skin_arr['data'] = array();
-
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
+$skin_arr['data'] = array();
 
     $skin_item = array(
 	'name' => $skin->name
@@ -31,14 +25,6 @@ if($num > 0){
 
     array_push($skin_arr['data'], $skin_item);
 
-}
-
-echo json_encode($skin_arr);
-
-}else{
-    echo json_encode(
-        array('message' => 'No results found')
-    );
-}
+print_r(json_encode($skin_arr));
 
 ?>
