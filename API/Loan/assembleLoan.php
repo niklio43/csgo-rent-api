d319638d54b8fff00eca38f6ef2141a189b8cf73
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-07-31 20:40:01 +0200

    Update assembleLoan.php

--- API/Loan/assembleLoan.php
+++ API/Loan/assembleLoan.php
@@ -2,8 +2,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../config/Database.php';
-include_once '../../models/loan.php';
+include_once '../../Config/Database.php';
+include_once '../../Models/loan.php';
 
 
 $database = new Database();
